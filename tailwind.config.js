4c13033656e9c85ea9bfa641e6c88fa708f823d9
Henrique Torres <ricalisboa@gmail.com>
2023-06-10 19:52:12 -0300

    feat - added donation links

--- tailwind.config.js
+++ tailwind.config.js
@@ -52,6 +52,7 @@ module.exports = {
         },
       },
       animation: {
+        overlayShow: 'overlayShow 150ms cubic-bezier(0.16, 1, 0.3, 1)',
         contentShow: 'contentShow 150ms cubic-bezier(0.16, 1, 0.3, 1)',
         contentHide: 'contentHide 300ms cubic-bezier(0.16, 1, 0.3, 1)',
       },
