17fe72492b74e9d8667b042c7db55b4121a7626b
HenriqueFLTorres <ricalisboa@gmail.com>
2023-07-07 21:37:22 -0300

    fix - testing serverless function sizes

--- api/index.py
+++ api/index.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS, cross_origin
-import cv2
-import numpy as np
+# import cv2
+# import numpy as np
 import utils
 import base64
 
@@ -49,19 +49,19 @@ def hello_world():
         
     originalImage = None  
 
-    try:
-        im_bytes = base64.b64decode(imageBase64)
-        im_arr = np.frombuffer(im_bytes, dtype=np.uint8)  # im_arr is one-dim Numpy array
-        originalImage = cv2.imdecode(im_arr, flags=cv2.IMREAD_COLOR)
-    except:
-        return jsonify({
-            'message': "Unsupported image.",
-            'status': 415
-        })
+    # try:
+    #     im_bytes = base64.b64decode(imageBase64)
+    #     im_arr = np.frombuffer(im_bytes, dtype=np.uint8)  # im_arr is one-dim Numpy array
+    #     originalImage = cv2.imdecode(im_arr, flags=cv2.IMREAD_COLOR)
+    # except:
+    #     return jsonify({
+    #         'message': "Unsupported image.",
+    #         'status': 415
+    #     })
 
-    originalImage = utils.addAlphaChannels(originalImage)
-    inventoryImage = originalImage.copy()
-    trade = cv2.imread(r"C:\Users\rical\Documents\VCS\Mir4Tools\public\items\trade.png", cv2.IMREAD_UNCHANGED)
+    # originalImage = utils.addAlphaChannels(originalImage)
+    # inventoryImage = originalImage.copy()
+    # trade = cv2.imread(r"C:\Users\rical\Documents\VCS\Mir4Tools\public\items\trade.png", cv2.IMREAD_UNCHANGED)
 
     # for item in items:
     #     itemTemplate = cv2.imread(rf"C:\Users\rical\Desktop\Testing\items\{item}.png", cv2.IMREAD_UNCHANGED)
@@ -73,16 +73,16 @@ def hello_world():
 
     #     utils.searchItem(itemTemplate, item, originalImage, tradeIcon, inventoryImage, PlayerInventory)
     
-    imageToJSON = inventoryImage.copy()
-    _, im_arr = cv2.imencode('.png', imageToJSON)  # im_arr: image in Numpy one-dim array format.
-    im_bytes = im_arr.tobytes()
-    im_b64 = base64.b64encode(im_bytes).decode("utf8")
+    # imageToJSON = inventoryImage.copy()
+    # _, im_arr = cv2.imencode('.png', imageToJSON)  # im_arr: image in Numpy one-dim array format.
+    # im_bytes = im_arr.tobytes()
+    # im_b64 = base64.b64encode(im_bytes).decode("utf8")
 
 
     return jsonify({
         'message': None,
-        'inventory': PlayerInventory,
-        'image': im_b64
+        # 'inventory': PlayerInventory,
+        # 'image': im_b64
     })
 
 if __name__ == '__main__':
