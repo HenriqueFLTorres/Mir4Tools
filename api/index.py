480ca9d1d76f51d59227cf1174edba62090667f0
HenriqueFLTorres <ricalisboa@gmail.com>
2023-07-09 12:05:41 -0300

    feat - removed inventory from image feature from website

--- api/index.py
+++ api/index.py
@@ -1,35 +1,39 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS, cross_origin
 import cv2
-# import numpy as np
+import numpy as np
+import imutils
+import pytesseract
 import utils
 import base64
 
-# items = [
-#     "anima_stone",
-#     "blue_devil_stone",
-#     # "copper",
-#     # "dark_steel",
-#     # "dragon_leather",
-#     # "energy",
-#     "evil_minded_orb",
-#     "exorcism_bauble",
-#     "glittering_powder",
-#     "illuminating_fragment",
-#     "moon_shadow_stone",
-#     "platinum",
-#     "quintessence",
-#     "steel",
-#     # "dragon_eye",
-#     # "dragon_scale",
-#     # "dragon_claw",
-#     # "dragon_horn",
-# ]
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract"
 
-# PlayerInventory = {}
-# GLOBAL_SCALE = 1.4
-# threshold = 0.85
-# frameOffset = int(np.round(30 * GLOBAL_SCALE))
+items = [
+    "anima_stone",
+    "blue_devil_stone",
+    # "copper",
+    # "dark_steel",
+    # "dragon_leather",
+    # "energy",
+    "evil_minded_orb",
+    "exorcism_bauble",
+    "glittering_powder",
+    "illuminating_fragment",
+    "moon_shadow_stone",
+    "platinum",
+    "quintessence",
+    "steel",
+    # "dragon_eye",
+    # "dragon_scale",
+    # "dragon_claw",
+    # "dragon_horn",
+]
+
+PlayerInventory = {}
+GLOBAL_SCALE = 1.4
+threshold = 0.85
+frameOffset = int(np.round(30 * GLOBAL_SCALE))
 
 app = Flask(__name__)
 CORS(app)
@@ -47,42 +51,42 @@ def hello_world():
             'status': 422
         })
         
-    # originalImage = None  
+    originalImage = None  
 
-    # try:
-    #     im_bytes = base64.b64decode(imageBase64)
-    #     im_arr = np.frombuffer(im_bytes, dtype=np.uint8)  # im_arr is one-dim Numpy array
-    #     originalImage = cv2.imdecode(im_arr, flags=cv2.IMREAD_COLOR)
-    # except:
-    #     return jsonify({
-    #         'message': "Unsupported image.",
-    #         'status': 415
-    #     })
+    try:
+        im_bytes = base64.b64decode(imageBase64)
+        im_arr = np.frombuffer(im_bytes, dtype=np.uint8)  # im_arr is one-dim Numpy array
+        originalImage = cv2.imdecode(im_arr, flags=cv2.IMREAD_COLOR)
+    except:
+        return jsonify({
+            'message': "Unsupported image.",
+            'status': 415
+        })
 
-    # originalImage = utils.addAlphaChannels(originalImage)
-    # inventoryImage = originalImage.copy()
-    # trade = cv2.imread(r"C:\Users\rical\Documents\VCS\Mir4Tools\public\items\trade.png", cv2.IMREAD_UNCHANGED)
+    originalImage = utils.addAlphaChannels(originalImage)
+    inventoryImage = originalImage.copy()
+    trade = cv2.imread(r"C:\Users\rical\Documents\VCS\Mir4Tools\public\items\trade.png", cv2.IMREAD_UNCHANGED)
 
-    # for item in items:
-    #     itemTemplate = cv2.imread(rf"C:\Users\rical\Desktop\Testing\items\{item}.png", cv2.IMREAD_UNCHANGED)
-    #     itemTemplate = itemTemplate[30:62, 30:62]
-    #     itemTemplate = imutils.resize(
-    #         itemTemplate, width=int(itemTemplate.shape[1] * GLOBAL_SCALE)
-    #     )
-    #     tradeIcon = imutils.resize(trade, width=int(trade.shape[1] * 0.90526))
+    for item in items:
+        itemTemplate = cv2.imread(rf"C:\Users\rical\Desktop\Testing\items\{item}.png", cv2.IMREAD_UNCHANGED)
+        itemTemplate = itemTemplate[30:62, 30:62]
+        itemTemplate = imutils.resize(
+            itemTemplate, width=int(itemTemplate.shape[1] * GLOBAL_SCALE)
+        )
+        tradeIcon = imutils.resize(trade, width=int(trade.shape[1] * 0.90526))
 
-    #     utils.searchItem(itemTemplate, item, originalImage, tradeIcon, inventoryImage, PlayerInventory)
+        utils.searchItem(itemTemplate, item, originalImage, tradeIcon, inventoryImage, PlayerInventory)
     
-    # imageToJSON = inventoryImage.copy()
-    # _, im_arr = cv2.imencode('.png', imageToJSON)  # im_arr: image in Numpy one-dim array format.
-    # im_bytes = im_arr.tobytes()
-    # im_b64 = base64.b64encode(im_bytes).decode("utf8")
+    imageToJSON = inventoryImage.copy()
+    _, im_arr = cv2.imencode('.png', imageToJSON)  # im_arr: image in Numpy one-dim array format.
+    im_bytes = im_arr.tobytes()
+    im_b64 = base64.b64encode(im_bytes).decode("utf8")
 
 
     return jsonify({
         'message': None,
-        # 'inventory': PlayerInventory,
-        # 'image': im_b64
+        'inventory': PlayerInventory,
+        'image': im_b64
     })
 
 if __name__ == '__main__':
--- src/components/crafting/Inventory.tsx
+++ src/components/crafting/Inventory.tsx
@@ -3,16 +3,12 @@ import BasicItemFrame from '@/components/Inventory/BasicItemComponent'
 import ItemComponent from '@/components/Inventory/ItemComponent'
 import Close from '@/icons/Close'
 import { useAtom } from 'jotai'
-import Image from 'next/image'
-import { useState } from 'react'
 import { useTranslation } from '../../../public/locales/client'
 
 export default function Inventory() {
   const [inventory] = useAtom(InventoryAtom)
   const [, setShowInventory] = useAtom(showInventoryAtom)
   const { t } = useTranslation()
-  const [image, setImage] = useState<undefined | string>(undefined)
-  const [result, setResult] = useState<undefined | string>(undefined)
 
   return (
     <>
@@ -29,66 +25,6 @@ export default function Inventory() {
           <Close className="fill-white" />
         </button>
       </header>
-
-      <label>
-        Upload
-        <input
-          type="file"
-          onChange={async (e) => {
-            const file = e.currentTarget.files?.[0]
-            if (!file) return
-            const data = await convertToBase64(file)
-
-            setImage(data as string)
-          }}
-        />
-      </label>
-
-      <button
-        onClick={async () => {
-          console.log('click')
-          try {
-            const headers = new Headers()
-            headers.append('Content-Type', 'application/json')
-
-            const response = await fetch(
-              'http://localhost:8000/api/inventoryFromImage',
-              {
-                method: 'POST',
-                headers,
-                body: JSON.stringify({
-                  image: image?.replace('data:image/png;base64,', ''),
-                }),
-                redirect: 'follow',
-              }
-            )
-            const data = await response.json()
-
-            setResult(data.image)
-          } catch (error) {
-            console.error(error)
-          }
-        }}
-      >
-        send
-      </button>
-
-      {image && (
-        <Image
-          src={`data:image/jpeg;base64,${image}`}
-          alt=""
-          width={693}
-          height={417}
-        />
-      )}
-      {result && (
-        <Image
-          src={`data:image/jpeg;base64,${result}`}
-          alt=""
-          width={693}
-          height={417}
-        />
-      )}
 
       <section className="mt-8 flex flex-col gap-8 p-4">
         {Object.entries(inventory).map(([name, rarities]) => (
