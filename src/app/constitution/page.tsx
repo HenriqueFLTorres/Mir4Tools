fffaced5e853bdf553f0767b9a38716c4cd72bce
HenriqueFLTorres <ricalisboa@gmail.com>
2023-07-31 11:23:53 -0300

    feat - added constitution tier handler

--- src/app/constitution/page.tsx
+++ src/app/constitution/page.tsx
@@ -1,5 +1,6 @@
 import ConstitutionCostInformation from '@/components/constitution/CostInformation'
 import ConstitutionStatsTable from '@/components/constitution/StatsTable'
+import TierHandler from '@/components/constitution/TierHandler'
 import ConstitutionWarning from '@/components/constitution/Warning'
 import ConstitutionBackground from '@/icons/ConstitutionBackground'
 import dynamic from 'next/dynamic'
@@ -14,6 +15,7 @@ export default function Constitution() {
     <div className="relative mx-auto flex h-screen w-full max-w-[90rem] flex-col items-center gap-8 px-6 pt-20 selection:bg-primary-800">
       <div className="-pt-24 relative z-10 flex scale-75 items-center justify-center md:mb-28 md:mt-8 md:scale-125 lg:scale-100">
         <ConstitutionStatusSelector />
+        <TierHandler />
         <ConstitutionBackground className="h-[20rem] w-[20rem] lg:h-auto lg:w-auto [&>#cbDots]:origin-center [&>#cbDots]:animate-rotate [&>#cbLabyrinth]:origin-center [&>#cbLabyrinth]:animate-rotate [&>#cbMiddle]:origin-center [&>#cbMiddle]:animate-rotateInvert" />
       </div>
 
---
+++ src/components/constitution/TierHandler.tsx
@@ -0,0 +1,59 @@
+'use client'
+import { statusLevelsAtom, type statusEffects } from '@/atoms/Constitution'
+import { useAtom } from 'jotai'
+
+export default function TierHandler() {
+  const [levels, setLevels] = useAtom(statusLevelsAtom)
+
+  const handleStageChange = (type: 'increment' | 'decrement') => {
+    const minLevel = Math.min(
+      ...Object.values(levels).map((values) => values.from)
+    )
+    const tier = Math.ceil(minLevel / 5) + 1
+
+    const newValues = Object.values(levels).map(({ from, to }) => ({
+      from: getValidNumber(5 * (type === 'increment' ? tier : tier - 2)),
+      to: getValidNumber(5 * (type === 'increment' ? tier + 1 : tier - 1)),
+    }))
+
+    const merged = Object.keys(levels).reduce(
+      (obj, key, index) => ({ ...obj, [key]: newValues[index] }),
+      {}
+    ) as {
+      [key in statusEffects]: { from: number; to: number }
+    }
+
+    setLevels(merged)
+  }
+
+  const minLevel = Math.min(
+    ...Object.values(levels).map((values) => values.from)
+  )
+  const currentTier = Math.ceil(minLevel / 5)
+
+  return (
+    <div className="absolute flex items-center text-base font-bold text-white">
+      <button
+        aria-label="Decrement constitution tier"
+        onClick={() => handleStageChange('decrement')}
+        className="rounded-l-full bg-primary-500 p-2 transition-colors hover:bg-primary-450"
+      >
+        -
+      </button>
+      <p className="bg-primary-500 p-2">Tier {currentTier}</p>
+      <button
+        aria-label="Increment constitution tier"
+        onClick={() => handleStageChange('increment')}
+        className="rounded-r-full bg-primary-500 p-2 transition-colors hover:bg-primary-450"
+      >
+        +
+      </button>
+    </div>
+  )
+}
+
+function getValidNumber(value: number) {
+  if (value < 1) return 1
+  if (value > 105) return 105
+  return value
+}
