86962499ca1eec7c1461b52f298b97510c37a142
HenriqueFLTorres <ricalisboa@gmail.com>
2023-10-16 18:14:43 -0300

    feat - add suggestion creditts

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,3 +1,8 @@
+function Anchor({ children, ...props }: { children: JSX.Element | JSX.Element[] } & React.HTMLAttributes<HTMLAnchorElement>) {
+  return <a {...props} className="font-bold cursor-pointer underline">{children}</a>
+}
+
+
 function H2({ children }: { children: JSX.Element | JSX.Element[] }) {
   return <h2 className="text-white">{children}</h2>
 }
@@ -7,5 +12,5 @@ function UL({ children }: { children: JSX.Element | JSX.Element[] }) {
 }
 
 export function useMDXComponents(components: JSX.Element | JSX.Element[]) {
-  return { ul: UL, h2: H2, ...components }
+  return { ul: UL, h2: H2, a: Anchor, ...components }
 }
