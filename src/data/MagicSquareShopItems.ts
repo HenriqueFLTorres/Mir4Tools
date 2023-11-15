930af30471c9d9209bda2d3961583cd39a0e3c5c
HenriqueFLTorres <ricalisboa@gmail.com>
2023-11-15 09:18:38 -0300

    feat - add magic square shop items data

--- src/data/MagicSquareShopItems.ts
+++ src/data/MagicSquareShopItems.ts
@@ -1,5 +1,233 @@
-const MagicSquareShopItems: MagicSquareShopItems = {
+const SpecialMaterial = {
+  '[E] Darksteel Box': {
+    '[E] Lunar Shard': 5,
+    '[E] Boundless Shard': 5,
+    Copper: 5000,
+  },
+  '[E] Energy Box': {
+    '[E] Ethereal Shard': 5,
+    '[E] Solar Shard': 5,
+    Copper: 5000,
+  },
+  '[E] Copper Box': {
+    '[E] Ethereal Shard': 1,
+    '[E] Lunar Shard': 1,
+    Darksteel: 100,
+  },
+  '[E] Glittering Powder Box': [
+    {
+      '[E] Ethereal Shard': 5,
+      Copper: 500,
+    },
+    {
+      '[E] Lunar Shard': 5,
+      Copper: 500,
+    },
+    {
+      '[E] Solar Shard': 5,
+      Copper: 500,
+    },
+    {
+      '[E] Boundless Shard': 5,
+      Copper: 500,
+    },
+  ],
+  '[R] Darksteel Box': {
+    '[R] Lunar Shard': 5,
+    '[R] Boundless Shard': 5,
+    Copper: 1000,
+  },
+  '[R] Energy Box': {
+    '[R] Ethereal Shard': 5,
+    '[R] Solar Shard': 5,
+    Copper: 1000,
+  },
+  '[R] Copper Box': {
+    '[R] Ethereal Shard': 1,
+    '[R] Lunar Shard': 1,
+    Darksteel: 50,
+  },
+  '[R] Glittering Powder Box': [
+    {
+      '[UC] Ethereal Shard': 30,
+      Copper: 500,
+    },
+    {
+      '[UC] Lunar Shard': 30,
+      Copper: 500,
+    },
+    {
+      '[UC] Solar Shard': 30,
+      Copper: 500,
+    },
+    {
+      '[UC] Boundless Shard': 30,
+      Copper: 500,
+    },
+    {
+      '[R] Ethereal Shard': 3,
+      Copper: 500,
+    },
+    {
+      '[R] Lunar Shard': 3,
+      Copper: 500,
+    },
+    {
+      '[R] Solar Shard': 3,
+      Copper: 500,
+    },
+    {
+      '[R] Boundless Shard': 3,
+      Copper: 500,
+    },
+  ],
+  '[UC] Darksteel Box': {
+    '[UC] Lunar Shard': 5,
+    '[UC] Boundless Shard': 5,
+    Copper: 500,
+  },
+  '[UC] Energy Box': {
+    '[UC] Ethereal Shard': 5,
+    '[UC] Solar Shard': 5,
+    Copper: 500,
+  },
+  '[UC] Copper Box': {
+    '[UC] Ethereal Shard': 1,
+    '[UC] Lunar Shard': 1,
+    Darksteel: 10,
+  },
+}
 
+const Equipment = {
+  'Glittering Powder': {
+    '[UC] Lunar Shard': 5,
+    Copper: 500,
+  },
+  '[UC] Steel': {
+    '[UC] Ethereal Shard': 1,
+    '[UC] Solar Shard': 1,
+    Copper: 500,
+    Darksteel: 200,
+  },
+  '[R] Steel': {
+    '[R] Ethereal Shard': 1,
+    '[R] Solar Shard': 1,
+    Copper: 1000,
+    Darksteel: 1000,
+  },
+  '[E] Steel': {
+    '[E] Ethereal Shard': 2,
+    '[E] Solar Shard': 2,
+    Copper: 5000,
+    Darksteel: 5000,
+  },
+  '[UC] Platinum': {
+    '[UC] Solar Shard': 1,
+    '[UC] Boundless Shard': 1,
+    Copper: 500,
+    Darksteel: 200,
+  },
+  '[R] Platinum': {
+    '[R] Solar Shard': 1,
+    '[R] Boundless Shard': 1,
+    Copper: 1000,
+    Darksteel: 1000,
+  },
+  '[E] Platinum': {
+    '[E] Solar Shard': 2,
+    '[E] Boundless Shard': 2,
+    Copper: 5000,
+    Darksteel: 5000,
+  },
+}
+
+const Promotion = {
+  '[UC] Moonlight Magic Stone': {
+    '[UC] Ethereal Shard': 3,
+    '[UC] Solar Shard': 2,
+    Copper: 500,
+    Darksteel: 200,
+  },
+  '[UC] Blue Devil Stone': {
+    '[UC] Lunar Shard': 3,
+    '[UC] Boundless Shard': 2,
+    Copper: 500,
+    Darksteel: 200,
+  },
+  '[UC] Purified Water': {
+    '[UC] Ethereal Shard': 3,
+    '[UC] Solar Shard': 2,
+    '[UC] Boundless Shard': 3,
+    Copper: 500,
+    Energy: 200,
+  },
+  '[UC] Virtue Pill': {
+    '[UC] Lunar Shard': 3,
+    '[UC] Solar Shard': 2,
+    '[UC] Boundless Shard': 3,
+    Copper: 500,
+    Energy: 200,
+  },
+  '[R] Moonlight Magic Stone': {
+    '[R] Ethereal Shard': 5,
+    '[R] Solar Shard': 5,
+    Copper: 1000,
+    Darksteel: 1000,
+  },
+  '[R] Blue Devil Stone': {
+    '[R] Lunar Shard': 5,
+    '[R] Boundless Shard': 5,
+    Copper: 1000,
+    Darksteel: 1000,
+  },
+  '[R] Purified Water': {
+    '[R] Ethereal Shard': 5,
+    '[R] Solar Shard': 5,
+    '[R] Boundless Shard': 5,
+    Copper: 1000,
+    Energy: 1000,
+  },
+  '[R] Virtue Pill': {
+    '[R] Lunar Shard': 5,
+    '[R] Solar Shard': 5,
+    '[R] Boundless Shard': 5,
+    Copper: 1000,
+    Energy: 1000,
+  },
+  '[E] Moonlight Magic Stone': {
+    '[E] Ethereal Shard': 5,
+    '[E] Lunar Shard': 5,
+    '[E] Solar Shard': 5,
+    Copper: 5000,
+    Darksteel: 5000,
+  },
+  '[E] Blue Devil Stone': {
+    '[E] Lunar Shard': 5,
+    '[E] Solar Shard': 5,
+    '[E] Boundless Shard': 5,
+    Copper: 5000,
+    Darksteel: 5000,
+  },
+  '[E] Purified Water': {
+    '[E] Ethereal Shard': 5,
+    '[E] Lunar Shard': 5,
+    '[E] Boundless Shard': 5,
+    Copper: 5000,
+    Energy: 5000,
+  },
+  '[E] Virtue Pill': {
+    '[E] Ethereal Shard': 5,
+    '[E] Lunar Shard': 5,
+    '[E] Boundless Shard': 5,
+    Copper: 5000,
+    Energy: 5000,
+  },
+}
+
+const MagicSquareShopItems = {
+  SpecialMaterial,
+  Equipment,
+  Promotion,
 }
 
 export default MagicSquareShopItems
