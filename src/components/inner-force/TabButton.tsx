e512117e2ac8cdfb7bdb0be552aaec41e1c15401
HenriqueFLTorres <ricalisboa@gmail.com>
2024-01-24 12:17:46 -0300

    feat - add final tier to tab button inner force

--- src/components/inner-force/TabButton.tsx
+++ src/components/inner-force/TabButton.tsx
@@ -25,12 +25,16 @@ export default function TabButton({
   const minLevel = Math.min(
     ...Object.values(targetedObject).map((values) => values.initial)
   )
+  const maxLevel = Math.min(
+    ...Object.values(targetedObject).map((values) => values.final)
+  )
   const currentTier = Math.round(minLevel / 5) + 1
+  const nextTier = Math.round(maxLevel / 5) + 1
 
   return (
     <button
       className={cn(
-        'flex w-20 h-20 sm:h-28 sm:w-28 shrink-0 flex-col justify-center rounded border-2 border-transparent bg-primary-600/50 px-3 py-2 text-white transition-colors hover:border-primary-400 hover:bg-primary-600/80 xl:h-20 xl:w-52 xl:flex-row xl:items-center xl:justify-between',
+        'flex h-20 w-20 shrink-0 flex-col justify-center rounded border-2 border-transparent bg-primary-600/50 px-3 py-2 text-white transition-colors hover:border-primary-400 hover:bg-primary-600/80 sm:h-28 sm:w-28 xl:h-20 xl:w-52 xl:flex-row xl:items-center xl:justify-between',
         { 'border-primary-400 bg-primary-600/80': tab === tabName },
         className
       )}
@@ -42,12 +46,16 @@ export default function TabButton({
         alt={tabName}
         width={70}
         height={58}
-        className="object-contain w-12 sm:w-[4.5rem]"
+        className="w-12 object-contain sm:w-[4.5rem]"
       />
       <div className="hidden flex-col justify-between text-center text-xs font-medium xl:flex xl:h-full xl:text-end">
         <p>
           Tier {currentTier}{' '}
-          {/* <span className="text-success-400">{'>'} Tier 5</span> */}
+          {nextTier > currentTier && (
+            <span className="text-success-400">
+              {'>'} Tier {nextTier}
+            </span>
+          )}
         </p>
         <p className="max-w-[6rem]">{tabName}</p>
       </div>
